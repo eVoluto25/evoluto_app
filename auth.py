cc31ffeccfe7349bf7a15463b2c007f6ee7230de
eVoluto25 <info@capitaleaziendale.it>
2025-05-29 12:31:22 +0200

    Add files via upload

--- auth.py
+++ auth.py
@@ -1,11 +1,8 @@
-import streamlit as st
+
 import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
-from datetime import datetime, timedelta
 
 # Crea hash delle password in chiaro
-hashed_passwords = stauth.Hasher(*["admin123", "cliente123"]).generate()
+hashed_passwords = stauth.Hasher(["admin123", "cliente123"]).generate()
 
 # Definizione utenti
 users = {
@@ -20,34 +17,3 @@ users = {
         "role": "cliente"
     }
 }
-
-# Configurazione Streamlit Authenticator
-config = {
-    'credentials': {
-        'usernames': {
-            username: {
-                'name': data['name'],
-                'password': data['password']
-            } for username, data in users.items()
-        }
-    },
-    'cookie': {
-        'expiry_days': 1,
-        'key': 'evoluto_login_cookie',
-        'name': 'evoluto_cookie'
-    },
-    'preauthorized': {
-        'emails': []
-    }
-}
-
-# Funzione di login
-def login_user():
-    authenticator = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days']
-    )
-    name, authentication_status, username = authenticator.login('Login', 'main')
-    return authenticator, name, authentication_status, username
