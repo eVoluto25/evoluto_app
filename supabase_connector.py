b44f04bf48f4c445446377e3a13731e5200f6739
eVoluto25 <info@capitaleaziendale.it>
2025-05-30 16:01:23 +0200

    Add files via upload

---
+++ supabase_connector.py
@@ -0,0 +1,12 @@
+
+from supabase import create_client, Client
+import os
+
+# Queste chiavi vanno definite come variabili d'ambiente o sostituite direttamente
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+def fetch_bandi():
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    response = supabase.table("bandi").select("*").execute()
+    return response.data if response.data else []
