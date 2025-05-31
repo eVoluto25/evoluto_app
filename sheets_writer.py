d74df196eb9eb3f91de7380ce5cc1d819c74aad7
eVoluto25 <info@capitaleaziendale.it>
2025-05-31 13:56:23 +0200

    Update sheets_writer.py

--- sheets_writer.py
+++ sheets_writer.py
@@ -13,7 +13,7 @@ def write_to_sheets(dati, azienda, macroarea):
     creds = get_google_credentials()
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
-    gpt_output = dai.get("gpt_output", {})
+    gpt_output = dati.get("gpt_output", {})
 
     # Scrittura dati aziendali
     valori_base = [
