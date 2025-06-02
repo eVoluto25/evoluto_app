730b33c189ddb06a1f5d938600a1cdfbec6cb935
eVoluto25 <info@capitaleaziendale.it>
2025-06-02 11:53:54 +0200

    Update drive_utils.py

--- drive_utils.py
+++ drive_utils.py
@@ -14,3 +14,18 @@ def upload_file_to_drive(filename, folder_id):
     file_drive.SetContentFile(filename)
     file_drive.Upload()
     return file_drive['id']
+
+def create_drive_subfolder(folder_name, parent_folder_id):
+    gauth = GoogleAuth()
+    gauth.LoadServiceConfigSettings()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    folder_metadata = {
+        'title': folder_name,
+        'mimeType': 'application/vnd.google-apps.folder',
+        'parents': [{'id': parent_folder_id}]
+    }
+    folder = drive.CreateFile(folder_metadata)
+    folder.Upload()
+    return folder['id']
