002a84634acce52ddd3fe839e4d6e5d4e61fb87f
eVoluto25 <info@capitaleaziendale.it>
2025-06-08 13:05:42 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ from simulatore_impatto import simula_beneficio
 from gpt_formatter import genera_snippet_analisi
 from impatto_simulato import calcola_impatto_simulato
 from pdf_cleaner import pulisci_pdf
-from bilancio import estrai_dati_bilancio
+from bilancio import estrai_dati_bilancio_completo
 from macroarea import assegna_macroarea
 from bandi_matcher import trova_bandi_compatibili
 from valutazione_punteggio import calcola_valutazione
