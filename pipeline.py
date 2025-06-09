2930f028b071acf02f92e9f6a4c4b7d41a7e4a1c
eVoluto25 <info@capitaleaziendale.it>
2025-06-09 15:36:18 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,8 +4,8 @@ import smtplib
 from email.mime.text import MIMEText
 from bandi_matcher import trova_bandi_compatibili
 from scoring_engine_ready import macroarea_match
-from output_gpt import genera_output_gpt, genera_commento_bandi
-from gpt_formatter import genera_snippet_analisi
+from output_gpt import genera_output_gpt
+from gpt_formatter import genera_commento_bandi
 from impatto_simulato import calcola_impatto_simulato, simula_beneficio, simula_impatto_totale
 from motivazione_bando import genera_motivazione_bando
 
