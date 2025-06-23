3779b5405116c3fee6760aad9598bf5c5f6526c6
eVoluto25 <info@capitaleaziendale.it>
2025-06-23 09:45:54 +0200

    Create indicatori.py

---
+++ indicatori.py
@@ -0,0 +1,10 @@
+def stima_z_score(bilancio: Bilancio):
+    if not bilancio.totale_attivo or bilancio.totale_attivo == 0:
+        return 0
+    return round((bilancio.ebitda + bilancio.utile_netto) / bilancio.totale_attivo, 2)
+
+
+def stima_mcc(bilancio: Bilancio):
+    if not bilancio.ricavi or bilancio.ricavi == 0:
+        return 0
+    return round((bilancio.utile_netto / bilancio.ricavi) * 100, 2)
