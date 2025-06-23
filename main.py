f7c4548d23719f9b06f7157fa2f41f4131ddad71
eVoluto25 <info@capitaleaziendale.it>
2025-06-23 09:47:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -66,17 +66,6 @@ class InputDati(BaseModel):
     bilancio: Bilancio
     risposte_test: RisposteTest
 
-# Indicatori economico-finanziari
-def stima_z_score(bilancio: Bilancio):
-    if not bilancio.totale_attivo or bilancio.totale_attivo == 0:
-        return 0
-    return round((bilancio.ebitda + bilancio.utile_netto) / bilancio.totale_attivo, 2)
-
-def stima_mcc(bilancio: Bilancio):
-    if not bilancio.ricavi or bilancio.ricavi == 0:
-        return 0
-    return round((bilancio.utile_netto / bilancio.ricavi) * 100, 2)
-
 def calcola_indici_plus(bilancio: Bilancio) -> dict:
     try:
         patrimonio_netto = bilancio.totale_attivo - sum([
