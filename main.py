235720d8d2d1a502f2c73ae41467b086598643cb
eVoluto25 <info@capitaleaziendale.it>
2025-06-23 10:32:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from models import Bilancio, Anagrafica, RisposteTest, InputDati
 from query_supabase import recupera_dettagli_bando
 from query_supabase import recupera_bandi_filtrati
 from query_supabase import somma_agevolazioni_macroarea
@@ -37,34 +38,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Modelli input
-class Anagrafica(BaseModel):
-    codice_ateco: Optional[str] = None
-    regione: Optional[str] = None
-    forma_giuridica: Optional[str] = None
-    numero_dipendenti: Optional[int] = None
-    attivita_prevalente: Optional[str] = None
-
-class Bilancio(BaseModel):
-    ricavi: Optional[float] = 0
-    utile_netto: Optional[float] = 0
-    ebitda: Optional[float] = 0
-    totale_attivo: Optional[float] = 0
-    immobilizzazioni: Optional[float] = 0
-    ricavi_anno_prec: Optional[float] = None
-
-class RisposteTest(BaseModel):
-    crisi_impresa: Optional[str] = None
-    sostegno_liquidita: Optional[str] = None
-    sostegno_investimenti: Optional[str] = None
-    transizione_ecologica: Optional[str] = None
-    innovazione_ricerca: Optional[str] = None
-
-class InputDati(BaseModel):
-    anagrafica: Anagrafica
-    bilancio: Bilancio
-    risposte_test: RisposteTest
 
 def calcola_indici_plus(bilancio: Bilancio) -> dict:
     try:
