7c6257fdfb3d125f8aaf49bd51ebc57a867a791e
eVoluto25 <info@capitaleaziendale.it>
2025-06-23 18:40:09 +0200

    Update indicatori.py

--- indicatori.py
+++ indicatori.py
@@ -10,3 +10,19 @@ def stima_mcc(bilancio: Bilancio):
     if not bilancio.ricavi or bilancio.ricavi == 0:
         return 0
     return round((bilancio.utile_netto / bilancio.ricavi) * 100, 2)
+
+def converti_z_score_lettera(z_score: float) -> str:
+    if z_score >= 2.5:
+        return "A"
+    elif 1.0 <= z_score < 2.5:
+        return "B"
+    else:
+        return "C"
+
+def converti_mcc_lettera(mcc: float) -> str:
+    if mcc >= 7:
+        return "A"
+    elif 4 <= mcc < 7:
+        return "B"
+    else:
+        return "C"
