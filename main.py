78dd74b0c4cad3ce8342be3421494cff1d827430
eVoluto25 <info@capitaleaziendale.it>
2025-06-25 20:53:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
 from fastapi import FastAPI
 from supabase import create_client, Client
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 
