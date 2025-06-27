4c83192d0d928383b0e21fdfe1ac4d0a6e4bd29a
eVoluto25 <info@capitaleaziendale.it>
2025-06-27 18:30:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class AziendaInput(BaseModel):
     dimensione: str  # Esempio: "Microimpresa", "Piccola Impresa", ecc.
     regione: str     # Esempio: "Lombardia", "Lazio", ecc.
     obiettivo_preferenziale: str  # Esempio: "Innovazione", "Sostegno", ecc.
+    settore_principale: str
     mcc_rating: str  # Esempio: "BBB", "AA", ecc.
     z_score: float   # Esempio: -1.2, 0.5, ecc.
 
