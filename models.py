ce440885045f8dcf27794e38e0a6400620030485
eVoluto25 <info@capitaleaziendale.it>
2025-06-27 14:15:15 +0200

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class AziendaInput(BaseModel):
-    dimensione: str  # Esempio: "Microimpresa", "Piccola Impresa", ecc.
-    regione: str     # Esempio: "Lombardia", "Lazio", ecc.
-    obiettivo_preferenziale: str  # Esempio: "Innovazione", "Sostegno", ecc.
-    mcc_rating: str  # Esempio: "BBB", "AA", ecc.
-    z_score: float   # Esempio: -1.2, 0.5, ecc.
