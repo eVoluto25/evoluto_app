460406f17f0539b958e30c77657aae6910c11db0
eVoluto25 <info@capitaleaziendale.it>
2025-08-01 11:23:02 +0200

    Create loader_prompt.py

---
+++ loader_prompt.py
@@ -0,0 +1,27 @@
+from fase_0 import FASE_0
+from fase_1 import FASE_1
+from fase_2 import FASE_2
+from fase_3 import FASE_3
+from fase_4 import FASE_4
+from fase_5 import FASE_5
+from fase_6 import FASE_6
+from fase_7 import FASE_7
+from fase_8 import FASE_8
+from fase_9 import FASE_9
+from fase_10 import FASE_10
+from fase_11 import FASE_11
+
+master_flow = {
+    "fase_0": FASE_0,
+    "fase_1": FASE_1,
+    "fase_2": FASE_2,
+    "fase_3": FASE_3,
+    "fase_4": FASE_4,
+    "fase_5": FASE_5,
+    "fase_6": FASE_6,
+    "fase_7": FASE_7,
+    "fase_8": FASE_8,
+    "fase_9": FASE_9,
+    "fase_10": FASE_10,
+    "fase_11": FASE_11,
+}
